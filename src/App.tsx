cae1d0302d2d275026113f2f8d186a23576b4035
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2024-12-27 00:02:54 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.defer = true;
+    script.dataset.siteId = 'test-react-cloudflare-production.up.railway.app';
+    script.src = 'https://pub-32e9964050ce412b937311b3b21de5ee.r2.dev/tracker.js';
+    document.body.appendChild(script);
+
+    // Очистка скрипта при розмонтуванні компонента
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,11 +31,6 @@ function App() {
         >
           Learn React
         </a>
-        <script 
-          defer 
-          data-site-id="test-react-cloudflare-production.up.railway.app" 
-          src="https://pub-32e9964050ce412b937311b3b21de5ee.r2.dev/tracker.js"> 
-        </script>
       </header>
     </div>
   );
